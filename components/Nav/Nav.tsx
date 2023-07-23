04f46371a2a49a01d6b50ff2a5a0660582d66a78
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-07-23 12:23:18 +0300

    create navigation

--- components/Nav/Nav.tsx
+++ components/Nav/Nav.tsx
@@ -1,7 +1,30 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { navData } from "@/data/navData";
+import styles from './Nav.module.scss'
+
 export const Nav = () => {
+    const router = useRouter();
+    const pathname = router.pathname;
+
     return (
-        <div>
-            Nav
-        </div>
+        <nav className={styles.nav}>
+            <div className={styles.nav__items}>
+                {navData.map((link, index) => {
+                    return <Link href={link.path} key={index}
+                                 className={`${styles.nav__items_link} ${link.path === pathname ? styles.link__active : ''}`}>
+                        <div className={styles.tooltip}>
+                            <div className={styles.tooltip__container}>
+                                <p className={styles.tooltip__container_text}>{link.name}</p>
+                                <div className={styles.tooltip__triangle}></div>
+                            </div>
+                        </div>
+                        <div>
+                            {link.icon}
+                        </div>
+                    </Link>
+                })}
+            </div>
+        </nav>
     );
 };
