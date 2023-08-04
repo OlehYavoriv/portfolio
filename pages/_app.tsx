1045b09a2d0618c86d88deb8a514cbc832dc0793
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-08-04 22:56:52 +0300

    create contact page, add nodemailer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,18 +3,21 @@ import { Layout } from "@/components/Layout";
 import { useRouter } from 'next/router';
 import { Transition } from "@/components/Transition";
 import { AnimatePresence, motion } from 'framer-motion';
+import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
     const router = useRouter();
     return (
-        <Layout>
-            <AnimatePresence mode='wait'>
-                <motion.div key={router.route}>
-                    <Transition/>
-                    <Component {...pageProps} />
-                </motion.div>
-            </AnimatePresence>
-        </Layout>
+        <ChakraProvider>
+            <Layout>
+                <AnimatePresence mode='wait'>
+                    <motion.div key={router.route}>
+                        <Transition/>
+                        <Component {...pageProps} />
+                    </motion.div>
+                </AnimatePresence>
+            </Layout>
+        </ChakraProvider>
     )
 }
 
